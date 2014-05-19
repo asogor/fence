5254fba4542835b488947db39e842928d18bf8ab
Aron Sogor <bigman@gmail.com>
2014-05-19 15:52:16 -0700

    Getting ready for first push

--- route/login.go
+++ route/login.go
@@ -9,6 +9,12 @@ type login struct {
 	config util.Config
 }
 
+type LoginContext struct {
+	Title string
+	FbAppId string
+	RedirectURL string
+}
+
 const TEMPLATE util.TemplateType = "login"
 
 func NewLogin(c util.Config) http.Handler {
@@ -16,5 +22,6 @@ func NewLogin(c util.Config) http.Handler {
 }
 
 func (l *login) ServeHTTP(w http.ResponseWriter, r *http.Request){
-	l.config.Template(TEMPLATE).Execute(w,nil)
+	l.config.Template(TEMPLATE).Execute(w,LoginContext{Title:"NonSense",FbAppId:"313331798817136",RedirectURL:"http://dev.bluefung.us:8000/fblogin"})
 }
+
--- util/config.go
+++ util/config.go
@@ -1,21 +1,23 @@
 package util
 
 import (
-	"log"
+	"encoding/json"
 	"html/template"
+	"io/ioutil"
+	"log"
 	"os"
 )
 
-type TemplateType string 
+type TemplateType string
 
-type config struct{
-	log *log.Logger
+type config struct {
+	log     *log.Logger
 	dataDir string
 }
 
 type Config interface {
 	Template(t TemplateType) (template *template.Template)
-	Log()(log *log.Logger)
+	Log() (log *log.Logger)
 }
 
 func (c *config) Log() (log *log.Logger) {
@@ -23,16 +25,36 @@ func (c *config) Log() (log *log.Logger) {
 }
 
 func (c *config) Template(ttype TemplateType) (t *template.Template) {
-	log.Println("Load Template", t)
+	c.log.Println("Load Template", t)
 	t, err := template.ParseFiles(c.dataDir + "/view/" + string(ttype) + ".html")
-	if(err != nil) {
-		log.Fatal("Template loading failed", err)
+	if err != nil {
+		c.log.Fatal("Template loading failed", err)
 	}
 	return t
 }
 
-func NewConfig(data string)(c Config){
-	l := log.New(os.Stdout,"FENCE",log.LstdFlags)
-	return &config{log:l,dataDir:data}
+func NewConfig(data string) (c Config) {
+	secret_config := data + "/config/secret"
+
+	l := log.New(os.Stdout, "FENCE ", log.LstdFlags)
+
+	l.Println("Start loading:", secret_config)
+	secretdata, err := ioutil.ReadFile(secret_config)
+	if err != nil {
+		l.Fatalln("Failed to load secret", secret_config)
+	}
+
+	var secret appSecret
+	err = json.Unmarshal(secretdata, &secret)
+	if err != nil {
+		panic(err)
+	}
+	l.Println("Secret loaded", secret)
+
+	return &config{log: l, dataDir: data}
 }
-	
+
+type appSecret struct {
+	FbAppId string
+	FbRedirectUrl string
+}
